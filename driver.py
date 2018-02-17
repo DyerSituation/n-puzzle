cd56661258524ddc4d2a98db956f645de379cf0c
Erik <erikdyer5@gmail.com>
2018-02-17 21:56:19 +0000

    hope you're working soon

--- driver.py
+++ driver.py
@@ -1,25 +1,75 @@
 import sys
 import time
+import math
 
 start_time = time.clock()
 searchType = sys.argv[1]
 grid = sys.argv[2].split(",")
 
-class BoardState(boardList):
+def swapPos(zeroPos, newPos, boardList):
+		tempList = list(boardList)
+		tempElement = tempList[zeroPos]
+		tempList[zeroPos] = tempList[newPos]
+		tempList[newPos] = tempElement
+		return tempList
+	
+def moveList(boardList, zeroPos):
+	#given list, produce list of acceptible lists
+	rowLen = int(math.sqrt(len(boardList)))
+	column = int(zeroPos % rowLen)
+	boards = []
+	#up
+	if(zeroPos > rowLen - 1):
+		newPos = zeroPos - rowLen
+		boards.append(swapPos(zeroPos, newPos, boardList))
+	#down
+	if(zeroPos < len(boardList) - 1):
+		newPos = zeroPos + rowLen
+		boards.append(swapPos(zeroPos, newPos, boardList))
+	#left
+	if(column > 0):
+		newPos = zeroPos - 1
+		boards.append(swapPos(zeroPos, newPos, boardList))
+	#right
+	if(column < rowLen - 1):
+		newPos = zeroPos + 1
+		boards.append(swapPos(zeroPos, newPos, boardList))
+	return boards
+
+class BoardState:
 	pathList = []
 	pathCost = 0
 	addX = 0
 
 	def __init__(self, boardList):
-		parent = None
-		child = boardList
-class solver():
-	ds = 
+		self.parent = None
+		self.child = boardList
+		
+class nodeQueue:
+	queue = []
+	elements = set([])
+	def add(self, x):
+		self.queue.append(x)
+		self.elements.add(x)
+		
+	def remove(self):
+		self.elements.discard(0)
+		return self.queue.pop(0)
+		
+class solver:
+	ds = []
 
 def main():
-	print searchType
-	for item in grid:
-		print item
+	print ("searchtype: ", searchType)
+	firstState = BoardState(grid)
+		
+	boardList = moveList(grid, 1)
+	for board in boardList:
+		for i in range (0,9):
+			print(board[i]),
+			if((i + 1) % 3 == 0):
+				print "\n"
+		print "\n done \n"
 
 if __name__ == "__main__":
 	main()
